2b3bb919c0872b1e9c3c624f50c07cd57ceeba34
Joe Atkins <atreides322@mac.com>
2020-12-13 16:43:25 -0600

    Day 02 - Part 2

--- 02/day02.py
+++ 02/day02.py
@@ -5,18 +5,29 @@ def main():
         passwords = [ {"rule": rule, "password": password } for rule, password in
                         [ x.split(":") for x in input.readlines()]]
 
-    valid = 0
+    valid_old = 0
+    valid_new = 0
+
     for entry in passwords:
+        password = entry['password'].strip()
         rule_parts = entry['rule'].split(' ')
         character = rule_parts[1]
         min, max = (int(x) for x in rule_parts[0].split('-',2))
 
-        count = len([ x for x in entry['password'] if x == character])
+        count = len([ x for x in password if x == character])
 
         if( count >= min and count <= max ):
-            valid+=1
+            valid_old+=1
 
-    print(valid)
+        pos1 = min - 1
+        pos2 = max - 1
+
+        if password[ pos1 ] != password[ pos2 ] and \
+            ( password[ pos1 ] == character or password[ pos2 ] == character ):
+            valid_new+=1
+
+    print(f"Old Rule: {valid_old}")
+    print(f"New Rule: {valid_new}")
 
 if __name__ == "__main__":
     main()
