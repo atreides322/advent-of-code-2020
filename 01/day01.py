92e61bf8e1482a3b3271f46970dcdff81945f120
Joe Atkins <atreides322@mac.com>
2020-12-13 14:37:54 -0600

    Day 01 - Part 2

--- 01/day01.py
+++ 01/day01.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 
+from functools import reduce
+
 def main():
     with open('input.txt') as input:
         numbers = [ int(x) for x in input.readlines()]
 
-    for number in numbers:
-        find_number = 2020 - number
+    print_answer( find(numbers, 2020, 2) )
+    print_answer( find(numbers, 2020, 3) )
 
-        if find_number in numbers:
-            return number * find_number
+def find(numbers, target, factors):
+    for number in numbers:
+        find_number = target - number
+
+        if factors == 2:
+            if find_number in numbers:
+                return [number, find_number]
+
+        else:
+            result = find(numbers, find_number, factors - 1)
+            if result:
+                return [number] + result
+
+def print_answer( solution ):
+    print( " + ".join(str(x) for x in solution), "=", sum(solution) )
+    print( " * ".join(str(x) for x in solution), "=", reduce((lambda x, y: x * y), solution ) )
 
 if __name__ == "__main__":
-    print(main())
+    main()
