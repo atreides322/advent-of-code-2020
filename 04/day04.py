cc5d59ae2aba3c11be31b22bf05124ad02ee0632
Joe Atkins <atreides322@mac.com>
2020-12-13 21:12:54 -0600

    Day 04 - Part 2

--- 04/day04.py
+++ 04/day04.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+
+rules = {
+    'byr': (lambda x: validate_num_range(x, 1920, 2002)),
+    'iyr': (lambda x: validate_num_range(x, 2010, 2020)),
+    'eyr': (lambda x: validate_num_range(x, 2020, 2030)),
+    'hgt': (lambda x: validate_height(x)),
+    'hcl': (lambda x: bool(re.fullmatch(r'#[0-9a-f]{6}', x))),
+    'ecl': (lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']),
+    'pid': (lambda x: bool(re.fullmatch(r'\d{9}', x))),
+    'cid': (lambda x: True)
+}
 
 # cid "optional"
-required = sorted(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'])
+required = [x for x in sorted(list(rules.keys())) if x != 'cid']
+
+def validate_num_range( value, min, max):
+    return value.isdigit() and int(value) >= min and int(value) <= max
+
+def validate_height( height ):
+    if height.endswith('cm'):
+        return validate_num_range( height[0:-2], 150, 193)
+
+    if height.endswith('in'):
+        return validate_num_range( height[0:-2], 59, 76)
+
+    return False
 
 def main():
 
@@ -31,7 +55,8 @@ def main():
 
 def validate(data):
     keys = [ key for key in sorted(data.keys()) if key != 'cid']
-    return keys == required
+
+    return keys == required and all([ rules[key](value) for key, value in data.items() ])
 
 if __name__ == "__main__":
     main()
